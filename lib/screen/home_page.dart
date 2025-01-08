8b58968c13d2def14783a55ebad4e3ddbeb2e038
nguyenquangtung004 <nguyenquangtung04.cv@gmail.com>
2025-01-08 16:31:11 +0700

    Bước 2: Sau khi tạo widget  custome dialog tiếp tục tạo home page và khai báo người chơi và hoạt động của họ

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/widgets/game_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,7 +9,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
+  List<GameButton> buttonList;
+  var player 1;
+  var player 2;
+  var activePlayer;
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
