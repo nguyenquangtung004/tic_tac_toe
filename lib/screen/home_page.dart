2fbf46d1e8f1cd4b04868413d3c6e15f11780d1c
nguyenquangtung004 <nguyenquangtung04.cv@gmail.com>
2025-01-08 16:42:59 +0700

    Bước 3: Sau khi khai báo người chơi thì tạo ra phương thức đầu tiên batDauVanMoi với thì ta sẽ xóa các nước đi của người chơi 1 và người chơi 2 xong sau đó ta khai báo biến để theo dõi lượt chơi hiện tại là active player  và khai báo lại các nút button mới

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -10,9 +10,29 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<GameButton> buttonList;
-  var player 1;
-  var player 2;
+  var player1;
+  var player2;
   var activePlayer;
+
+  List<GameButton> batDauVanMoi(){
+    player1 = new List();
+    player2 = new List();
+    activePlayer = 1;
+
+    var gameButtons = <GameButton>[
+      new GameButton(id: 1),
+      new GameButton(id: 2),
+      new GameButton(id: 3),
+      new GameButton(id: 4),
+      new GameButton(id: 5),
+      new GameButton(id: 6),
+      new GameButton(id: 7),
+      new GameButton(id: 8),
+      new GameButton(id: 9),
+    ];
+    return gameButtons;
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
